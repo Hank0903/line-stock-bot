13e7b31b6ef2c6a0332785706f93797214ec6e4a
Hank0903 <aa0921399375@gmail.com>
2025-05-08 14:37:23 +0800

    debug

--- utils.py
+++ utils.py
@@ -13,5 +13,7 @@ def get_recent_trading_days(n=30):
 
 def date_to_query_format(date_obj):
     # 將 '20250428' 轉換成 '20250401'（月初）
+    if isinstance(date_obj, str):
+        date_obj = datetime.datetime.strptime(date_obj, '%Y%m%d')
     #return date_str[:6] + '01'
     return date_obj.strftime('%Y%m') + '01'
